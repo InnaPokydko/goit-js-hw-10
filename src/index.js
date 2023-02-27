eee9533a2d9fc62912fc5767584291c5bbd329da
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-27 12:40:47 +0200

    misteks http

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 import './css/styles.css';
-
+import nameCountry from 'https://restcountries.com/v3.1/name/{name}';
 import API from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
-
 const refs = {
-    inputForm: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
+  inputForm: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
 refs.inputForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const form = e.currentTarget;
-    const searchQuery = form.elements.query.value;
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
 
-    fetchCountries(searchQuery)
+  API.fetchCountries(searchQuery)
     .then(renderCountries)
-    .catch(error => console.log(error));
-
-    API.fetchCountries()
-  .then(renderCountries)
-  .catch(onFetchError)
-  .finally(() => form.reset());
+    .catch(onFetchError)
+    .finally(() => form.reset());
 }
 
 function renderCountries(country) {
-
+  const murkUp = nameCountry(nameId);
+  refs.inputForm.innerHTML = murkUp;
 }
 
 function onFetchError(error) {
-
+  Notiflix.Notify.failure(`❌Sory, there is no country with that name`);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 export default { fetchCountries };
 
-function fetchCountries(countryId) {
-  return fetch('https://restcountries.com/v3.1/name/${countryId}https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages')
+function fetchCountries() {
+  return fetch('https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages')
   .then(response => {
     return response.json();
   });
